778fa9196f3c2108196c2d089a3c15b4fa4f5a5e
Vaishnav Mankar <vmankar2910@gmail.com>
2025-09-23 21:49:47 +0530

    Problem on Decorator

---
+++ 14_Decorators/177_example_2.py
@@ -0,0 +1,30 @@
+#only_int_allow
+
+from functools import wraps
+
+def only_int_allow(function):
+    @wraps(function)
+    def wrapper(*args, **kwargs):
+        if all([type(arg) == int for arg in args]):
+            return function(*args, **kwargs) 
+        print("Invalid argument")
+    #     data_types = []
+    #     for arg in args:
+    #          data_types.append(type(arg)==int)
+    #     if all(data_types):
+    #         return function(*args,**kwargs)
+    #     else:
+    #         return "Invalid Argument"
+    return wrapper       
+
+@only_int_allow
+def add_all(*args):
+    total = 0
+    for i in args:
+        total += i
+    return total
+
+#print(add_all(1,2,3,4,5,[1,2,3]))
+print(add_all(1,2,3,4,5))
+print(add_all(1,2,3,4,5,"Varun"))
+
