7e62fabaf0aa6bee38966cd9ddb29b59044b3edf
Vaishnav Mankar <vmankar2910@gmail.com>
2025-11-01 23:58:07 +0530

    Setter Decorator

---
+++ 16_OOP/200_setter_decorator.py
@@ -0,0 +1,32 @@
+#will discuss three problems in existing
+#then will solve them using getting, setter decorator
+
+class Phone:
+    def __init__(self, brand, model_name, price):
+        self.brand = brand
+        self.model_name = model_name
+        # if price > 0:
+        #     self._price = price
+        # else:
+        #     self._price = 0
+        self._price = max(price, 0)
+        #self.complete_specification = f"{self.brand} {self.model_name} and price is {self._price}"
+
+    def complete_specification(self):
+            return f"{self.brand} {self.model_name} and price is {self._price}"
+
+
+    def make_a_call(self, phone_name):
+            print(f"calling {phone_name} ...")
+
+    def full_name(self):
+            return f"{self.brand} {self.model_name}"
+        
+
+phone1 = Phone('nokia','1100',1000)
+print(phone1.brand)
+print(phone1.model_name)
+phone1._price = 500
+print(phone1._price)
+
+print(phone1.complete_specification())
