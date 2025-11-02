d0ee23df78f87b54b62d4559c74c00dcd9d6e38e
Vaishnav Mankar <vmankar2910@gmail.com>
2025-11-02 23:28:46 +0530

    Updated 39_String Immutable code

--- 2_Basic operator and calculation/39_stringImmutable_1.py
+++ 2_Basic operator and calculation/39_stringImmutable_1.py
@@ -2,6 +2,6 @@
 
 string = "string"
 print(string.replace('t','T'))
-#this mesns iven if you want to chande something in the string 
+#this mesns even if you want to chande something in the string 
 print(string)#it will create a new string but the original string will remain the same
 
