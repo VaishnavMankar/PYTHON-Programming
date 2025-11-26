0615a4b53ec02d44ea33c267418b06f5cf26cfcf
VaishnavMankar <vmankar2910@gmail.com>
2025-11-26 14:17:39 +0530

    Build in errors

---
+++ 17_Advance_OPP/205_built_in_errors.py
@@ -0,0 +1,37 @@
+# build in errors
+# exception, how to handle them
+# raise your own errors, debugging etc etc
+
+#syntax error
+
+#def func:    This happen we you write code incurectly 
+#     pass     here function should have taken some input
+
+# indentation errors
+# def func():          This basecally meand you made mistake is spacing 
+#     print('Hello')   this line was currect but the command below should have statret 
+#   print('Hello')     from the left most index
+# indentation means mistake in geving space.
+
+# name error
+# print(name)  This is a name error because the namve variable was not declare outside
+# or the name vairable was empty or it was not declared
+
+# type error
+# print(5 + "Hello") This error happend when you are doing wrong operation on wrong type
+# here you are addign a integer to a string 
+
+# index error 
+# l = [1,2,3] This happens when you try to axcess value at the wrong index
+# print(l[4])
+
+# value error
+# a = '123'   It happens when you try to convert wrong data type into another 
+# b = 'abc'   here its a string and it contain alphabate you can convert it to int or float
+# print(int(a))
+# print(int(b)) So here it is creating valeu error because you cant convert letters into integers
+
+# attribute error 
+# l = [1,2,3,4] This happens when you use function which dosent exist
+# l.push('12')  Here push function dosent exist in python 
+
